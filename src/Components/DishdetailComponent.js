0e3487cf1660b962aad190ef9745e2b9eb7115e9
Khusal Paliwal <kpaliwaltm@gmail.com>
2020-07-01 00:40:11 +0530

    fetch from json server

--- src/Components/DishdetailComponent.js
+++ src/Components/DishdetailComponent.js
@@ -3,6 +3,8 @@ import { Card, CardImg, CardBody, Col, CardText, Row, CardTitle, Breadcrumb, Bre
 import { Link } from 'react-router-dom';
 import { Control, LocalForm, Errors } from 'react-redux-form';
 import { Loading } from './LoadingComponent';
+import {baseUrl} from '../shared/baseUrl';
+
 
 const minLength = (len) => (val) => val && (val.length >= len);
 const maxLength = (len) => (val) => !(val) || (val.length <= len);
@@ -135,7 +137,7 @@ function RenderComments({comments, addComment, dishId})
 			return(
 				<div className="col-12 col-md-5 m-1">
 				<Card>
-				   <CardImg top src={ dish.image } alt={dish.name}></CardImg> 
+				   <CardImg top src={baseUrl + dish.image } alt={dish.name}></CardImg> 
 				   <CardBody>
 					   <CardTitle>{ dish.name }</CardTitle>
 					   <CardText> { dish.description } </CardText>
